a810f06f6f73391fe7cbcdf4505382cb3c107693
Thomas Young <yk_ecust_2007@163.com>
2019-10-08 23:08:54 +0800

    [doc] 添加工程方法的一些笔记

--- src/headfirst/designpatterns/factory/factory_method/PizzaStore.java
+++ src/headfirst/designpatterns/factory/factory_method/PizzaStore.java
@@ -1,6 +1,11 @@
 package headfirst.designpatterns.factory.factory_method;
 
 public abstract class PizzaStore {
+    /**
+     * 这是工厂方法
+     * @param type
+     * @return
+     */
     protected abstract Pizza createPizza(String type);
     public Pizza orderPizza(String type) {
         Pizza pizza = createPizza(type);
