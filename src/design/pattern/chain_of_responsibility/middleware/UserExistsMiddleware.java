9833e6c62e31fcee57696eb4b0b60d65a7bf1d71
杨恺 <yangkai02@qutoutiao.net>
2020-06-28 11:11:17 +0800

    Chain

---
+++ src/design/pattern/chain_of_responsibility/middleware/UserExistsMiddleware.java
@@ -0,0 +1,25 @@
+package design.pattern.chain_of_responsibility.middleware;
+
+import design.pattern.chain_of_responsibility.server.Server;
+
+public class UserExistsMiddleware extends Middleware {
+    private Server server;
+
+    public UserExistsMiddleware(Server server) {
+        this.server = server;
+    }
+    @Override
+    public boolean check(String email, String password) {
+        if (!server.hasEmail(email)) {
+            System.out.println("This email is not registered!");
+            return false;
+        }
+        if (!server.isValidPassword(email, password)) {
+            System.out.println("Wrong password!");
+            return false;
+        }
+        boolean ret = checkNext(email, password);
+        System.out.println("UserExistsMiddleware post");
+        return ret;
+    }
+}
