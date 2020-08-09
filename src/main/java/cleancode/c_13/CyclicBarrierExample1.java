12ab961a65af69f75746551bc81e1e58de9f732d
杨恺 <yangkai02@qutoutiao.net>
2020-08-09 08:44:27 +0800

    semaphore

---
+++ src/main/java/cleancode/c_13/CyclicBarrierExample1.java
@@ -0,0 +1,39 @@
+package cleancode.c_13;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class CyclicBarrierExample1 {
+    private volatile static int a = 0;
+    private static CyclicBarrier barrier = new CyclicBarrier(5, () -> {
+        a++;
+    });
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executor.execute(() -> {
+                try {
+                    race(threadNum);
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+            });
+        }
+        executor.shutdown();
+    }
+
+    private static void race(int threadNum) throws Exception {
+        log.info("{} is ready", threadNum);
+        barrier.await();
+        log.info("{} continue, a={}", threadNum, a);
+    }
+}
